0d8faa0ff676157a954f6df452ed105530300d8e
HyunJoon,Kim <kimkuma04@gmail.com>
2020-07-26 12:23:49 +0900

    Vue portfolio update

--- client/src/store/home.js
+++ client/src/store/home.js
@@ -7,21 +7,50 @@ const home = {
                 id: 1,
                 content:
                 `
-                    <h1>Hello World</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+                    <h1>OBJECTIVE</h1>
+                    <p>웹 개발자로 서버사이드 개발 및 프론트엔드 개발 경험이 있습니다.<br/>
+                       Java, Spring Framework, Spring boot, JavaScript, Git, MySQL, MariaDB, Oracle, React.js, AWS 등의 기술을 사용하여 개발 및 운영한 경험이 있습니다.<br/>
+                       빠르게 발전하는 IT 업계에서 뒤쳐지지 않기 위해서 새로운 기술의 흐름을 파악하려고 노력하고 있습니다.
+                    </p>
                 `,
             },
             {
                 id: 2,
                 content:
                 `
-                    <h2>Second level</h2>
-                    <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
+                    <h2>SKILLS</h2>
+                    <p>FRONT-END</p>
                     <ul>
-                        <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
-                        <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
-                        <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
-                        <li>Ut non enim metus.</li>
+                        <li>JavaScript</li>
+                        <li>React.js</li>
+                        <li>Thymeleaf</li>
+                        <li>JSTL</li>
+                    </ul>
+                    <p>BACK-END</p>
+                    <ul>
+                        <li>Java</li>
+                        <li>Spring Framework</li>
+                        <li>RESTful API</li>
+                        <li>APACHE</li>
+                        <li>Mybatis</li>
+                        <li>Hibernate</li>
+                        <li>AWS</li>
+                    </ul>
+                    <p>DATABASE</p>
+                    <ul>
+                        <li>MySQL</li>
+                        <li>MariaDB</li>
+                        <li>Oracle</li>
+                        <li>Tibero</li>
+                    </ul>
+                    <p>ETC</p>
+                    <ul>
+                        <li>Ubuntu</li>
+                        <li>Git/GitHub/GitLab</li>
+                        <li>Subversion</li>
+                        <li>IntelliJ</li>
+                        <li>ActiveMQ</li>
+                        <li>Maven/Gradle</li>
                     </ul>
                 `,
             },
