342618f105f34ba72f8689088d403f35ef03724f
HyunJoon <kimkuma04@gmail.com>
2020-08-20 09:36:27 +0900

    포트포리오 내용 수정

--- client/src/store/home.js
+++ client/src/store/home.js
@@ -45,7 +45,6 @@ const home = {
                     </ul>
                     <p>ETC</p>
                     <ul>
-                        <li>lucy-xss-filter</li>
                         <li>Ubuntu</li>
                         <li>Git/GitHub/GitLab</li>
                         <li>Subversion</li>
